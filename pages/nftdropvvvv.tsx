5381e280c9f390c02512462564487d915bbe3047
Joshua V Arnaez <jarnaez@yondu.com>
2023-03-11 18:19:05 +0800

    ...

---
+++ pages/nftdropvvvv.tsx
@@ -0,0 +1,15 @@
+// //!!this code is still in development.....
+// import React from "react";
+// import Navbar from "@/components/layout/Navbar";
+// import MainHome from "@/components/home";
+
+// const Home = () => {
+//   return (
+//     <div className="min-h-screen bg-gray-800">
+//       <Navbar />
+//       <MainHome />
+//     </div>
+//   );
+// };
+
+// export default Home;
